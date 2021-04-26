9f1258967425a36ae525e842685753975dacc13f
Guhan Sensam <guhansensam@gmail.com>
2021-04-26 22:57:38 +0530

    Added full example

--- examples/full_example/main.py
+++ examples/full_example/main.py
@@ -1,19 +1,22 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+import CardStack
+from kivy.clock import Clock
+from kivy.uix.button import Button
 
-root_kv = """
-#:import ExampleButton kivymd_extensions.example_extension.ExampleButton
-
-BoxLayout:
-    ExampleButton:
-        text: "Click me!"
-"""
-
-
-class MainApp(MDApp):
+class Mainapp(MDApp):
     def build(self):
-        self.root = Builder.load_string(root_kv)
+        self.test = CardStack.CardStack()
+        self.test.pos_hint = {'center_x':.5, "center_y":.5}
+        self.test.size = (800,800)
+        return self.test
 
+    def on_start(self):
+        Clock.schedule_interval(self.runner, 2)
 
+    def runner(self, *args):
+        self.test.change()
+        self.test.current_card.add_widget(Button(size_hint = (.5,.5) )
+                                                                )
 if __name__ == "__main__":
     MainApp().run()
